3d1e1d8c561940ef3538f6e96e7859ffaa58d78c
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-01-21 06:02:49 +0000

    Create string is number.c

---
+++ string is number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+ char str[50];
+ int i,len = 0,count = 0;
+ printf("enter any string");
+ scanf("%s",str);
+ len = strlen(str);
+ for(i=0;i<len;i++)
+ {
+ if(str[i] >= o&& str[i] <= 9)    
+ {
+ count++;
+ }
+ }
+ printf("yes");
+ }
+	
