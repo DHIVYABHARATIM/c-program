42530f4faf00f26f1881625e87ba21ac4bce4d87
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-01-28 16:10:51 +0530

    Create special character .c

---
+++ special character .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i,count=0;
+    int str[i];
+    printf("enter the string");
+    for(i=0;a[i]!=null;i++)
+    {
+        if((a[i]>='0'&&a[i]<='9'));
+        count++;
+    }
+}
