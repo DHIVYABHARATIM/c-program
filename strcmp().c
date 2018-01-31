c39978bff85e26bb5846a85118992b144305e90a
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-01-31 15:57:55 +0530

    Create strcmp().c

---
+++ strcmp().c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int c,d;
+char a[30],b[30];
+printf("enter the strings");
+scanf("%s",a);
+sacnf("%s",b);
+c=strlen(a);
+d=strlen(b);
+{
+if(c>d)
+printf("%s",a);
+else if(d>c)
+printf("%s",b);
+else
+printf("%s%s",a,b);
+}
+}
