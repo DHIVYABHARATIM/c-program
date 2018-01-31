13783deba45e2da9159204ed852528fcc8684bdc
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-01-31 16:54:35 +0530

    Update small , large num.c

--- small , large num.c
+++ small , large num.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int a[10],temp,i,j,n;
+int a[1000],temp,i,j,n;
 for(i=0;i<10;i++)
 {
 scanf("%d",&a[i]);
 }
-for(i=0;i<10;i++)
+for(i=0;i<n;i++)
 { 
-for(j=i+1;j<=10;j++)
+for(j=i+1;j<=n;j++)
 {
 if(a[i]>a[j])
 {
