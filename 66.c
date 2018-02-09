3a50fab91f527c9ce1b987e1ccfc104eaee13a7f
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-02-09 16:59:09 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+ int i,n,flag=0;
+ scanf("%d",&n);
+ for(i=2;i<=n/2;i++)
+ {
+   if((n%i==0))
+   flag++;
+ }
+ if(flag==0)
+  printf("prime");
+  else
+  printf("not prime");
+ }
