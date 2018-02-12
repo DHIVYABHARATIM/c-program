ea7215e073fa05afa4d82d71f58088f0ea7e58aa
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-02-12 16:20:55 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int main(void)
+{
+ int i,n,a;
+ printf("enter the number");
+ scanf("%d",&n);
+if((n&(n-1))==0)
+{
+  printf("the num is power of 2\n");
+  a=n*2;
+  printf("the nearest power of 2 is %d",a);
+}
+else
+  printf("the num is not the power of 2");
+}  
+
+
+
+
