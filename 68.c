de2314f74df53ccd5816886d28c353e2b53eeffc
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-02-12 15:51:56 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+int main(void)
+{
+ int i,n,a;
+ printf("enter the number");
+ scanf("%d",&n);
+ if((n%7)==0)
+ {
+   printf("multiples of 7");
+ }
+ else
+ {
+   printf("no");
+ }
+}
