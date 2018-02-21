26e15999cde956a7ae2c11aa56f86e59478cd7cc
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-02-21 16:57:10 +0530

    Create perfect square.c

---
+++ perfect square.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main(void)
+{
+  int i,a,b;
+  printf("enter the num");
+  scanf("%d%d",&a,&b);
+  i=a*b;
+    if(b==a)
+      printf("yes");
+      else
+  printf("no");
+}
