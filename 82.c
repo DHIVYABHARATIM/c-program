22f6df5e345630f88f0992402a15cf494f773097
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-02-28 15:58:48 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main(void)
+{
+  float l,b,a;
+  printf("enter the length");
+  scanf("%f",&l);
+  printf("enter the breadth");
+  scanf("%f",&b);
+  a=l*b;
+  printf("the area is %.5f",a);
+}
+  
