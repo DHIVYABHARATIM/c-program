96f55feae066fd90bc4a7cc2af48253e602112f4
DHIVYABHARATIM <31499436+DHIVYABHARATIM@users.noreply.github.com>
2018-03-14 16:33:18 +0530

    Create 114.c

---
+++ 114.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+  int n,k,a[100],i,count=0;
+  printf("enter the  2  numbers");
+  scanf("%d%d",&n,&k);
+  printf("enter the numbers for checking");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    if((a[i]%2)!=0)
+    {
+        count++;
+        if(count==k)
+        printf("%d",a[i]);
+      }
+  }
+}
+  
+
